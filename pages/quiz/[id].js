6416dae655db8c92c9b42a302fa828d800528191
Victor <vtrcarvalho.13@gmail.com>
2021-01-29 17:35:00 -0300

    ..

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-export default function QuizDaGaleraPage() {
-    return <div>Desafio</div>;
+export default function QuizDaGaleraPage(props) {
+    return <div>{JSON.stringify(props)}</div>;
 }
 
+//executando no servidor
 export async function getServerSideProps(context) {
+    const externDB = await fetch()
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+
+            throw new Error('Erro ao buscar os dados na API');
+        })
+        .then((response) => {
+            return response;
+        })
+        .catch((err) => {
+            console.error(err);
+            return false;
+        });
     return {
-        props: {},
+        props: { externDB },
     };
 }
