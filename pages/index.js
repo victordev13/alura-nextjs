7cc72c5ed0daa265f0a28e802f971d9d8a562554
victordev13 <vtrcarvalho.13@gmail.com>
2021-05-20 19:44:45 -0300

    comment box

--- pages/index.js
+++ pages/index.js
@@ -13,58 +13,56 @@ import { motion } from 'framer-motion';
 import Link from '../src/components/Link';
 
 export default function Home() {
-    const router = useRouter();
-    const [name, setName] = React.useState('');
+  const router = useRouter();
+  const [name, setName] = React.useState('');
 
-    const handleSubmitName = (e) => {
-        e.preventDefault();
-        router.push(`/quiz?name=${name}`);
+  const handleSubmitName = (e) => {
+    e.preventDefault();
+    router.push(`/quiz?name=${name}`);
 
-        savePlayerName(name);
-    };
+    savePlayerName(name);
+  };
 
-    return (
-        <QuizBackground backgroundImage={db.bg}>
-            <QuizContainer>
-                <QuizLogo />
-                <Widget
-                    as={motion.section}
-                    variants={{
-                        show: { opacity: 1, y: '0' },
-                        hidden: { opacity: 0, y: '100%' },
-                    }}
-                    initial="hidden"
-                    animate="show"
-                    transition={{ delay: 0, duration: 0.5 }}
-                >
-                    <Widget.Header>
-                        <h1>{db.title}</h1>
-                    </Widget.Header>
-                    <Widget.Content>
-                        <p>{db.description}</p>
-                        <form onSubmit={handleSubmitName}>
-                            <Input
-                                placeholder="Seu nome"
-                                name="NomeDoJogador"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                maxLength="10"
-                                autoComplete="off"
-                            />
-                            <Button
-                                as={motion.button}
-                                whileHover={{ scale: 1.01 }}
-                                whileTap={{ scale: 0.99 }}
-                                type="submit"
-                                disabled={name.length === 0}
-                            >
-                                {` Jogar ${name}`}
-                            </Button>
-                        </form>
-                    </Widget.Content>
-                </Widget>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget
+          as={motion.section}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+          transition={{ delay: 0, duration: 0.5 }}>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+            <form onSubmit={handleSubmitName}>
+              <Input
+                placeholder="Seu nome"
+                name="NomeDoJogador"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                maxLength="10"
+                autoComplete="off"
+              />
+              <Button
+                as={motion.button}
+                whileHover={{ scale: 1.01 }}
+                whileTap={{ scale: 0.99 }}
+                type="submit"
+                disabled={name.length === 0}>
+                {` Jogar ${name}`}
+              </Button>
+            </form>
+          </Widget.Content>
+        </Widget>
 
-                <Widget
+        {/* <Widget
                     as={motion.section}
                     variants={{
                         show: { opacity: 1, y: '0' },
@@ -74,7 +72,7 @@ export default function Home() {
                     animate="show"
                     transition={{ delay: 0.2, duration: 0.5 }}
                 >
-                    {/* <Widget.Content>
+                    <Widget.Content>
                         <h1>Veja outros quizes</h1>
                         <ul>
                             {db.external.map((item, index) => {
@@ -96,24 +94,24 @@ export default function Home() {
                                 );
                             })}
                         </ul>
-                    </Widget.Content> */}
-                </Widget>
-                <Footer
-                    as={motion.section}
-                    variants={{
-                        show: { opacity: 1, y: '0' },
-                        hidden: { opacity: 0, y: '100%' },
-                    }}
-                    initial="hidden"
-                    animate="show"
-                    transition={{ delay: 0.4, duration: 0.5 }}
-                />
-            </QuizContainer>
-            <GitHubCorner projectUrl="https://github.com/victordev13/alura-nextjs" />
-        </QuizBackground>
-    );
+                    </Widget.Content>
+                </Widget> */}
+        <Footer
+          as={motion.section}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+          transition={{ delay: 0.4, duration: 0.5 }}
+        />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/victordev13/alura-nextjs" />
+    </QuizBackground>
+  );
 }
 
 function savePlayerName(name) {
-    localStorage.setItem('player', name);
+  localStorage.setItem('player', name);
 }
